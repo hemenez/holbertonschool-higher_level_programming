1adf17841d26ff4bc952b1d06d7de2af5297ad0f
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-04 22:32:52 +0000

    Removed dual NULL check condition

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 
 	single = list;
 	dubble = list->next;
-	while (single->next != NULL && dubble->next != NULL)
+	while (dubble->next != NULL)
 	{
 		if (single->next == dubble->next)
 			return (1);
