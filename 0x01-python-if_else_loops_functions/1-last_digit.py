6cf56ebe0440599b4a7202fe01536f574df529c4
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-05 21:18:25 +0000

    Wrote program to find value of last digit in number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print('Last digit of', '{:d}'.format(number), 'is ', end="")
+if number >= 0:
+    newnum = number % 10
+elif number < 0:
+    newnum = number * -1
+    newnum = newnum % 10
+    newnum = -newnum
+newnum = int(newnum)
+print(newnum, '', end="")
+if newnum > 5:
+    print('and is greater than 5')
+elif newnum == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
