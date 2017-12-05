71629a5a6ef63c40425d87b09898fa9aa534aac8
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-05 21:19:46 +0000

    Wrote program to print smallest combo of two digit numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(0, 10):
+    for num2 in range(0, 10):
+        if num < num2:
+            print('%d%d' % (num, num2), end="")
+            if num < 8 or num2 < 9:
+                print(', ', end="")
+            else:
+                print('\n', end="")
