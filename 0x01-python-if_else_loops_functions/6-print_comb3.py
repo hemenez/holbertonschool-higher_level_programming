d5b6acb9f2f6e0cbed5cd264ce63e4186e247340
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 04:43:10 +0000

    Removed typecast

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for num in range(0, 10):
     for num2 in range(0, 10):
         if num < num2:
-            print('%d%d' % (num, num2), end="")
+            print('{:d}{:d}'.format(num, num2), end="")
             if num < 8 or num2 < 9:
                 print(', ', end="")
             else:
