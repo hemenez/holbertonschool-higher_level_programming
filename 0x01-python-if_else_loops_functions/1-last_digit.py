897e6fb7e30d6e4539beb99d1c4d561c0472d48b
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 05:19:41 +0000

    Condensed print statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,9 @@ elif number < 0:
     newnum = number * -1
     newnum = newnum % 10
     newnum = -newnum
-print('{:d}'.format(newnum), '', end="")
 if newnum > 5:
-    print('and is greater than 5')
+    print('{:d}'.format(newnum), '', 'and is greater than 5')
 elif newnum == 0:
-    print('and is 0')
+    print('{:d}'.format(newnum), '', 'and is 0')
 else:
-    print('and is less than 6 and not 0')
+    print('{:d}'.format(newnum), '', 'and is less than 6 and not 0')
