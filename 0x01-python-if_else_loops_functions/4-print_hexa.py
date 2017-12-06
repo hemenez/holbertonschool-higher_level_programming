9bca0ef5bf24aef0d32e155c830023409b5f1e91
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 23:01:41 +0000

    Removed typecast

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(0, 99):
-    print(num, '', end="")
-    print('=', '', end="")
+    print('{:d}'.format(num), end="")
+    print('', '=', '', end="")
     print('{:#x}'.format(num))
