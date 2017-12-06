fbda8eb1713d74654a27958a95967b434d32fcde
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 04:21:57 +0000

    Wrote function to print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+def print_last_digit(number):
+    if number >= 0 and number < 10:
+        newnum = number
+    elif number > 9:
+        newnum = number % 10
+    else:
+        newnum = number * -1
+        newnum = newnum % 10
+    newnum = int(newnum)
+    print(newnum, end="")
+    return newnum
