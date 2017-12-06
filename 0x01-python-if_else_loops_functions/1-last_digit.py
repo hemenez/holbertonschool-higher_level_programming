9f2b01dd340a0cc864fd7b9587a7ebcdf8202b6d
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 05:30:32 +0000

    Added whitespace

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -14,5 +14,5 @@ elif newnum == 0:
     print('Last digit of', '{:d}'.format(number), 'is', '{:d}'.format(newnum),
           'and is 0')
 else:
-    print('Last digit of', '{:d}'.format(number), 'is','{:d}'.format(newnum),
+    print('Last digit of', '{:d}'.format(number), 'is', '{:d}'.format(newnum),
           'and is less than 6 and not 0')
