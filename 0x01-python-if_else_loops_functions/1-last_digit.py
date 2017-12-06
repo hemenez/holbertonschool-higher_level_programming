3000d0a7493ef7b027fe3d6f2a18bda8fd7defda
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 04:25:48 +0000

    Accounted for zero case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 print('Last digit of', '{:d}'.format(number), 'is ', end="")
-if number >= 0:
+if number > 0:
     newnum = number % 10
 elif number < 0:
     newnum = number * -1
     newnum = newnum % 10
     newnum = -newnum
+else:
+    newnum = number
 newnum = int(newnum)
 print(newnum, '', end="")
 if newnum > 5:
