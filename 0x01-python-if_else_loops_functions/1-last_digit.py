b7031c2805d30de730d789e2e7121d04448ba845
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 05:23:36 +0000

    Added temp holder for original value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 print('Last digit of', '{:d}'.format(number), 'is ', end="")
-if number >= 0:
-    newnum = number % 10
-elif number < 0:
-    newnum = number * -1
-    newnum = newnum % 10
+x = number
+if x < 0:
+    x = -x
+newnum = x % 10
+if number < 0:
     newnum = -newnum
 if newnum > 5:
     print('{:d}'.format(newnum), '', 'and is greater than 5')
