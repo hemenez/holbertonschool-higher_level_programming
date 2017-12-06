fc319fecfdcf3cf039236652d510f56bff18d6be
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-06 23:03:12 +0000

    Removed extra print statements

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for num in range(0, 99):
-    print('{:d}'.format(num), end="")
-    print('', '=', '', end="")
-    print('{:#x}'.format(num))
+    print('{:d}'.format(num), '=', '{:#x}'.format(num))
