728b43daf6717da8e7661ea7e4c12142969350a9
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 03:20:38 +0000

    Converted list to tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -21,4 +21,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             pass
     newnew = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-    return newnew
+    final = tuple(newnew)
+    return final
