e9c809cf9b44748726f73d9c75491a9fe84272b1
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 03:38:27 +0000

    Added condition for empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    char = sentence[0]
-    return (length, char)
+    if not sentence:
+        return None
+    else:
+        length = len(sentence)
+        char = sentence[0]
+        return (length, char)
