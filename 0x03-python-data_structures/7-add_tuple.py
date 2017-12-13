611ecf45015c8d4f78052f67780f0af0c7dd6bab
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 06:54:11 +0000

    Reimplemented function to utilize tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,9 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = list(tuple_a)
-    tuple_b = list(tuple_b)
-    if not tuple_a:
-        tuple_a = [0, 0]
-    if not tuple_b:
-        tuple_b = [0, 0]
-    for i in tuple_a:
-        if len(tuple_a) < 2:
-            tuple_a.append(0)
-        elif len(tuple_a) > 2:
-            del tuple_a[i]
-        else:
-            pass
-    for j in tuple_b:
-        if len(tuple_b) < 2:
-            tuple_b.append(0)
-        elif len(tuple_b) > 2:
-            del tuple_b[j]
-        else:
-            pass
-    newnew = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-    final = tuple(newnew)
-    return final
+    tuple_c = (0, 0)
+    tuple_a1 = tuple_a + tuple_c
+    tuple_b1 = tuple_b + tuple_c
+    tuple_a2 = tuple_a1[:2]
+    tuple_b2 = tuple_b1[:2]
+    total = ((tuple_a2[0] + tuple_b2[0]), (tuple_a2[1] + tuple_b2[1]))
+    return total
