151cd870b8397f0715dceb398e6486bd2102d05d
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 04:08:59 +0000

    Removed dual condition and added empty list checkk

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or not idx:
+    if not my_list:
+        return None
+    if idx < 0:
         return my_list
     else:
         for i in my_list:
