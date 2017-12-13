6318c986044abc48d640e931d06b00b0e73c4d73
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 01:32:32 +0000

    Removed semicolon from continue statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     renovated = ""
     for i in my_string:
         if i == "c" or i == "C":
-            continue;
+            continue
         else:
             renovated = renovated + i
     return renovated
