8616bd3e2b8de72294c6439dadc5ece8c056081c
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 01:31:23 +0000

    Scrapped function using lists to utilize string formats

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        pass
-    else:
-        new_string = list(my_string)
-        x = new_string.count("C")
-        y = new_string.count("c")
-        if x != 0:
-            xi = new_string.index("C")
-            del new_string[xi]
-            removedstr = new_string
-        if y != 0:
-            yi = new_string.index("c")
-            del new_string[yi]
-            removedstr = new_string
-        renovated = ''.join(map(str, removedstr))
-        return renovated
+    renovated = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            continue;
+        else:
+            renovated = renovated + i
+    return renovated
