de007005319ebd3a68f3fe7596745d2a1c295605
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 04:07:23 +0000

    Adjusted for condition when index is out of range

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,12 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or not idx:
         return my_list
-    for i in my_list:
-        if i == idx:
-            del my_list[i]
-        else:
-            pass
+    else:
+        for i in my_list:
+            if i == idx:
+                del my_list[i]
+            elif idx < i:
+                return my_list
+            else:
+                pass
     return my_list
