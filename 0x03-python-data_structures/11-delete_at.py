ddc8ad9dabf19c130335580718de1bfb63200363
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 04:33:21 +0000

    Added condition for test case of index 0

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not my_list or idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
+    elif idx == 0:
+        del my_list[0]
     else:
         for i in my_list:
             if i == idx:
