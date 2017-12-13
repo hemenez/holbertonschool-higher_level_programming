d921313a21149165d5ac06292e07f87c7188e5ff
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 01:16:17 +0000

    Added else statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         pass
     my_list.reverse()
-    for i in my_list:
-        print('{:d}'.format(i))
+    else:
+        for i in my_list:
+            print('{:d}'.format(i))
