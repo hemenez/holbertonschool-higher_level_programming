511939b3fb7fa2a1419bfe06da8034e8e6ef80eb
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 00:06:38 +0000

    Wrote a function to remove chars in string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = list(my_string)
+    x = new_string.count("C")
+    y = new_string.count("c")
+    if x != 0:
+        xi = new_string.index("C")
+        del new_string[xi]
+        removedstr = new_string
+    if y != 0:
+        yi = new_string.index("c")
+        del new_string[yi]
+        removedstr = new_string
+    renovated = ''.join(map(str,removedstr))
+    return renovated
