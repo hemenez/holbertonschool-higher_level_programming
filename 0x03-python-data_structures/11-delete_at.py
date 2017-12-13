6f68cb6856d00fc2d4e14a0c9ed2af14cdecb8ab
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-13 04:14:35 +0000

    Changed idx counter condition to be in if statement

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not my_list:
-        return None
-    if idx < 0:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
         for i in my_list:
             if i == idx:
                 del my_list[i]
-            elif idx < i:
-                return my_list
             else:
                 pass
     return my_list
