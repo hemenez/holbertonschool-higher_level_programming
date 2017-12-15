3408f5fc14f89126a3e8e09db8f1cc51b1c472c5
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-12-15 00:04:48 +0000

    Removed empty string condition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return None
-    else:
-        final = 0
-        my_list = set(my_list)
-        for num in sorted(my_list):
-            final += num
-        return final
+    final = 0
+    my_list = set(my_list)
+    for num in sorted(my_list):
+        final += num
+    return final
