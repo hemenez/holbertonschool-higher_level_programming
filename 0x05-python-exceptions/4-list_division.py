29057aba37e19fa19c901b29ca6e9b3926fc0f60
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-03 06:34:08 +0000

    Wrote a function to divide list elements

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    if my_list_1 is None or my_list_2 is None:
+        return new_list
+    else:
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                new_list.append(result)
+            except ZeroDivisionError:
+                result = 0
+                new_list.append(result)
+                print('division by 0')
+            except IndexError:
+                result = 0
+                new_list.append(result)
+                print('out of range')
+            except (TypeError, ValueError):
+                result = 0
+                new_list.append(result)
+                print('wrong type')
+        return new_list
+#        for i, j in zip(my_list_1, my_list_2):
+#           result = i / j
+#           new_list.append(result)
