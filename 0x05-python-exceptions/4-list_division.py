f750618157c14a0340c8a28ede12957a0348a397
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-03 06:40:34 +0000

    Added 'finally' statement

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,4 +20,6 @@ def list_division(my_list_1, my_list_2, list_length):
                 result = 0
                 new_list.append(result)
                 print('wrong type')
+            finally:
+                pass
         return new_list
