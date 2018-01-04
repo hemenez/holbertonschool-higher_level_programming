521743af94f5c76c48e123397f64c88c48021e4b
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-04 23:43:52 +0000

    Added condition checks for length of tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,11 +28,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple):
+        if type(value) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError('position must be a tuple of 2 positive integers')
         if value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) > 2:
+        if len(value) - 1 != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
