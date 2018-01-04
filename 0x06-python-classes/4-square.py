6120c73d6533c5da5a090bdc62788acf1bc41378
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-04 02:50:41 +0000

    Added property setter and property to retrieve size of square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines the size of a square with a private attribute
+    making sure that size is of an integer value and greater than zero and
+    returns the area size of the square. Retrieves value to set size of square
+    """
+    def __init__(self, __size=0):
+        self.__size = __size
+        try:
+            if type(__size) is not int:
+                raise TypeError
+            if __size < 0:
+                raise ValueError
+        except TypeError:
+            print('size must be an integer', end="")
+            raise
+        except ValueError:
+            print('size must be >= 0', end="")
+            raise
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            if type(value) is not int:
+                raise TypeError
+            if value < 0:
+                raise ValueError
+            else:
+                self.__size = value
+        except TypeError:
+            print('size must be an integer', end="")
+            raise
+        except ValueError:
+            print('size must be >= 0', end="")
+            raise
+
+    def area(self):
+        return int(self.__size) ** 2
