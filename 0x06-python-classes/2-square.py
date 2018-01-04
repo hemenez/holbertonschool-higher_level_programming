651fe210ff6765fd342fb7eea791051e4e5307a9
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-04 02:36:47 +0000

    Wrote a class defining a square with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines the size of a square with a private attribute
+    making sure that size is of an integer value and greater than zero"""
+    def __init__(self, __size=0):
+        self.__size = __size
+        try:
+            if type(__size) is not int:
+                raise TypeError
+            if __size < 0:
+                raise ValueError
+        except TypeError:
+            print('size must be an integer', end="")
+            raise
+        except ValueError:
+            print('size must be >= 0', end="")
+            raise
