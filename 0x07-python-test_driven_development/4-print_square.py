24c7a72a500e8421919645f983a6c48f7cd6ad0c
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-08 06:44:15 +0000

    Added docstring

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_square(size):
-    """ """
+    """Module will print a square w/special character"""
     if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
     if not isinstance(size, int):
