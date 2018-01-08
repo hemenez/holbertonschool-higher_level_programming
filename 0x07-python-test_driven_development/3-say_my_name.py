b0618d553c4044c5cc806614e54d724c2be340a4
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-08 02:07:20 +0000

    Wrote a function to print a name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """ """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print('My name is ', end="")
+    for i in range(len(first_name)):
+        if first_name[i] == ' ':
+            continue
+        else:
+            print(first_name[i], end="")
+    print(' ', end="")
+    for j in range(len(last_name)):
+        if last_name[j] == ' ':
+            continue
+        else:
+            print(last_name[j], end="")
+    print()
+#    print('{} {}'.format(first_name, last_name))
