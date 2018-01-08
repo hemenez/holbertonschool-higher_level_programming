ea5efd9c54f23e7e515ecae92b53a81c6ad30a0e
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-08 06:43:42 +0000

    Added docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
-    """ """
+    """Module will print a name following a string"""
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
