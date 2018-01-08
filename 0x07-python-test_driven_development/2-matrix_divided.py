0002cd3095f5b860ebea627edfeb11d0e7fd7869
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-08 06:02:46 +0000

    Fixed pep8 style error

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,19 +8,29 @@ def matrix_divided(matrix, div):
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError('div must be a number')
     if not isinstance(matrix, list):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats'
+        )
     if not isinstance(matrix[0], list):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats'
+        )
     for i in range(len(matrix)):
         row_count = len(matrix[0])
         lil_list = []
         for j in range(len(matrix[i])):
-            if not isinstance(matrix[i][j], float) and not isinstance(matrix[i][j], int):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            if not isinstance(matrix[i][j], float)
+            and not isinstance(matrix[i][j], int):
+                raise TypeError(
+                    'matrix must be a matrix' +
+                    ' (list of lists) of integers/floats'
+                )
             total = matrix[i][j] / div
             total = round(total, 2)
             lil_list.append(total)
-            if row_count != len(matrix[i]):
-                raise TypeError('Each row of the matrix must have the same size')
+        if row_count != len(matrix[i]):
+            raise TypeError(
+                'Each row of the matrix must have the same size'
+            )
         biggie_list.append(lil_list)
     return biggie_list
