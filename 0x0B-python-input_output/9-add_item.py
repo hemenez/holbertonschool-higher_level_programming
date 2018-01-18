b4e9b7377b13b566bef89fdabedc85ad486089bc
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-18 05:16:08 +0000

    Created first version of script that adds all args to a Python list

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+
+load_from_json_file(filename)
+
+with open("add_item.json", "a+", encoding="utf-8") as my_file:
+    my_list = []
+    index = 0
+    for i in sys.argv:
+        if sys.argv[index] == 0:
+            pass
+        else:
+            my_list.append(sys.argv[index])
+        index = index + 1
+
+save_to_json_file(my_list, filename)
