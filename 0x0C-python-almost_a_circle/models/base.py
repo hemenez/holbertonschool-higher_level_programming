958b56e5314c3df9cf49b4abf1a5f211c8f464c6
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-22 22:50:44 +0000

    Added method to write JSON string to a JSON file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,32 @@ class Base:
         if len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Method writes JSON string representation of a list
+        of objects to a file
+        """
+        new_dict = {}
+        final_list = []
+        final_dict = {}
+        if list_objs is None:
+            final_list = []
+        else:
+            for instance in list_objs:
+                new_dict = instance.__dict__
+                final_dict = {}
+                for key in new_dict:
+                    if "y" in key:
+                        final_dict["y"] = new_dict[key]
+                    if "x" in key:
+                        final_dict["x"] = new_dict[key]
+                    if "id" in key:
+                        final_dict["id"] = new_dict[key]
+                    if "width" in key:
+                        final_dict["width"] = new_dict[key]
+                    if "height" in key:
+                        final_dict["height"] = new_dict[key]
+                final_list.append(final_dict)
+        with open(cls.__name__ + ".json", 'w+', encoding="UTF-8") as f:
+            f.write(Base.to_json_string(final_list))
