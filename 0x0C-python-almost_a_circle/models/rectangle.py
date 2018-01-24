e6ce16aeb57168c3adec53a80d3ab7a676a27468
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-01-24 04:31:10 +0000

    Added docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """inherits from Base
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Method initializes values for rectangle class
