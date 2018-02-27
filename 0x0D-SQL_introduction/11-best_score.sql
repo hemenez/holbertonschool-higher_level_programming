f0f6ab926175d35b042dbf5d10494f9c629bbc68
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-02-27 02:35:52 +0000

    Capitalized where

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- Script lists records of the bable where score >= 10
 -- Score is ordered by highest first
 -- Script also lists name
-SELECT score, name FROM second_table where score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
