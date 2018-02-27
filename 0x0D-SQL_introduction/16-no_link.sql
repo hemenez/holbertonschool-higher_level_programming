55d943262eced8aeae2fcc736e2f7a9947f5afb4
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-02-27 03:04:32 +0000

    Added condition to handle name column being NULL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script lists all records of table unless there's no name value
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
