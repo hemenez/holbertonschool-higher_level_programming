9ed56193a3f32d903ae27551fc742a7750350529
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-01 02:23:20 +0000

    Removed mentioning of both tables

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script lists all cities of California that can be found in database
 -- results are sorted in ascending order by cities.id
-SELECT cities.id, cities.name FROM cities, states WHERE states.name='California' ORDER BY cities.id ASC;
+SELECT cities.id, cities.name FROM cities WHERE state_id=1 ORDER BY cities.id ASC;
