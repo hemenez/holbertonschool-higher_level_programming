edde70ca8ee498bbab660fd0b31bec82287af4f1
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-01 00:07:27 +0000

    Added second and third parameter

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 -- User will have all privileges on MySQL server
 -- If user already exists, script won't fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'users_0d_1'@'localhost';
+FLUSH PRIVILEGES;
