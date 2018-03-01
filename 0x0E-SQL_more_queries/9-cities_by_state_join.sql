bdb1fbe906642ccacc0b05cba7dc2e944cb0248d
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-01 02:31:52 +0000

    Wrote script to list all cities contained in database using join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script lists all cities contained in database
+-- record displays city id, name and state name
+-- ordered by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id ASC;
