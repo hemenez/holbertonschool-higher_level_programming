356463e783f4d58c863848948c3a72aec970acdd
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2018-03-01 05:57:16 +0000

    Added renaming of genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- doesn't display a genre that has no shows linked
 -- record displays genre title and number of shows
 -- results sorted in desc order by number of shows linked
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id)
-AS 'number_shows'
+SELECT tv_genres.name AS 'genre',
+COUNT(tv_show_genres.genre_id) AS 'number_shows'
 FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
