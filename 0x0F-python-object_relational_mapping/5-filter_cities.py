de11710a9c844cbb73835dcf49aff5117960f625
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-03-23 20:20:37 +0000

    Changed formatting of print

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     AND states.name = %s\
     ORDER BY cities.id", (search,))
     my_result = cur.fetchall()
+    my_str = ""
     count = 0
     for combo in my_result:
-        count += 1
-        if combo[1] == search:
-            if count != len(my_result):
+            if count != len(my_result) - 1:
                 print(combo[0] + ', ', end="")
             else:
                 print(combo[0])
+            count += 1
     cur.close()
     db.close()
