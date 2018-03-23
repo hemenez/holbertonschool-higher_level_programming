e5539be0cba3f14d897efa847c4fac692322d852
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-03-23 19:15:40 +0000

    Script displays all values in table with given argument on CL

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    res = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id".format(res))
+    my_states = cur.fetchall()
+    for state in my_states:
+        print(state)
+    cur.close()
+    db.close()
