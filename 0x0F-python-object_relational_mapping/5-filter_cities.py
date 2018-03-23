e07498de50bbf8f43a114b20b0eda69fbac34b50
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-03-23 20:33:46 +0000

    Adjusted query syntax

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name, states.name\
     FROM cities INNER JOIN states\
     ON cities.state_id = states.id\
-    AND states.name = %s\
+    WHERE states.name=%s\
     ORDER BY cities.id", (search,))
     my_result = cur.fetchall()
     count = 0
