09e4af33aff25f1eab8911fd6caaaa534828183e
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-03-23 20:04:42 +0000

    Script deletes all State objects with name containing a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+from sqlalchemy import create_engine, delete
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    result = session.query(State).filter(State.name.op('regexp')('a'))
+    result.delete(synchronize_session=False)
+    session.commit()
+    session.close()
