d84d91ce76568367671ac60fe4a8d7f4331aae04
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-03-23 19:55:53 +0000

    Added check to make sure result matches desired input

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=password,
                          db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT cities.name\
+    cur.execute("SELECT cities.name, states.name\
     FROM cities INNER JOIN states\
     ON cities.state_id = states.id\
     AND states.name = %s\
@@ -21,9 +21,10 @@ if __name__ == "__main__":
     count = 0
     for combo in my_result:
         count += 1
-        if count != len(my_result):
-            print(combo[0] + ', ', end="")
-        else:
-            print(combo[0])
+        if combo[1] == search:
+            if count != len(my_result):
+                print(combo[0] + ', ', end="")
+            else:
+                print(combo[0])
     cur.close()
     db.close()
