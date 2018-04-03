4a324e05dfdbf96e5ac21c538fa44f4c1f6591e4
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-03 20:52:40 +0000

    Removed max statement

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,7 @@
 def find_peak(list_of_integers):
     """ Fxn finds peak in list of integers """
     if list_of_integers is not None:
-#        num = list_of_integers
-#        for idx in range(1, len(num)):
-#            if num[idx] > num[idx + 1] and num[idx] > num[idx - 1]:
-#                return num[idx]
-#        else:
-        return max(list_of_integers)
+        num = list_of_integers
+        for idx in range(1, len(num)):
+            if num[idx] > num[idx + 1] and num[idx] > num[idx - 1]:
+                return num[idx]
