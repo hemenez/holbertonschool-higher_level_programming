c0e272358eee8c95b5b1273c3f71fd8d06dfcc08
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-03 20:42:45 +0000

    Testing max fxn for time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    if list_of_integers is not None or len(list_of_integers) > 1:
-        my_list = list_of_integers
-        for idx in range(1, len(my_list)):
-            if my_list[idx] > my_list[idx + 1] and my_list[idx] > my_list[idx - 1]:
-                return my_list[idx]
-        else:
-            return max(my_list)
+    """ Fxn finds peak in list of integers """
+    if list_of_integers is not None:
+#        num = list_of_integers
+#        for idx in range(1, len(num)):
+#            if num[idx] > num[idx + 1] and num[idx] > num[idx - 1]:
+#                return num[idx]
+#        else:
+        return max(num)
