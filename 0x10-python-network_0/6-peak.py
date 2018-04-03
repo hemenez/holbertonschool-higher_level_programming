68bd015237b52c98f388f1637a0ca91f21c33bd2
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-03 20:34:59 +0000

    Added checks for if list is of correct size

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    my_list = list_of_integers
-    for idx in range(1, len(my_list)):
-        if my_list[idx] > my_list[idx + 1] and my_list[idx] > my_list[idx - 1]:
-            return my_list[idx]
+    if list_of_integers is not None or len(list_of_integers) > 1:
+        my_list = list_of_integers
+        for idx in range(1, len(my_list)):
+            if my_list[idx] > my_list[idx + 1] and my_list[idx] > my_list[idx - 1]:
+                return my_list[idx]
+        else:
+            return max(my_list)
