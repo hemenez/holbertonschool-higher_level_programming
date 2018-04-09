615f15621d928a53f67cc2d6b3db03e56a5b7db9
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-09 18:51:17 +0000

    Wrote a script to retrieve error codes and/or body response from URL

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Script takes in URL, sends request to it, &displays body of response
+import sys
+import urllib.request
+import urllib.error
+url = sys.argv[1]
+try:
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        print(data.decode('utf-8'))
+except urllib.error.HTTPError as e:
+    print('Error code:', e.code)
