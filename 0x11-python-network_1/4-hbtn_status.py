b831b75c5b0ad3645b39dbe0d0aacaa3783cc71a
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-09 19:14:48 +0000

    Removed whitespace

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     response = requests.get(url)
     print('Body response:')
     print('\t- type:', type(response.text))
-    print ('\t- content:', response.text)
+    print('\t- content:', response.text)
