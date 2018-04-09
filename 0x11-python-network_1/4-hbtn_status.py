90f622ce7b9a9fc7721fd8654a618688c8e0f362
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-09 19:05:23 +0000

    Removed extra space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     response = requests.get(url)
     print('Body response:')
-    print('\t - type:', type(response.text))
-    print ('\t - content:', response.text)
+    print('\t- type:', type(response.text))
+    print ('\t- content:', response.text)
