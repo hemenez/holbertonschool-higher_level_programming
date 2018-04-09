477ef4d159cc47b3689a7f987df8f87450c751eb
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-09 19:35:28 +0000

    Broke apart print statement

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,4 +5,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers['X-Request-Id'])
+    data = response.headers['X-Request-Id']
+    print(data)
