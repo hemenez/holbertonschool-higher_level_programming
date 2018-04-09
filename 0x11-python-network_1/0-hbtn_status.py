286c5b86a75481d2e28bb0551fb22893bbb859ce
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-09 18:50:12 +0000

    Wrote a script to fetch a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Script fetches URL
+import urllib.request
+import urllib.error
+url = 'https://intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+    print('Body response:')
+    print('\t- type:', type(data))
+    print('\t- content:', data)
+    print('\t- utf8 content:', data.decode('utf-8'))
