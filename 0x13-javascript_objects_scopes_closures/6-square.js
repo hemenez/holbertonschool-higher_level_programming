7096e976c7f906bc819cc99a1beeaf38da905459
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-05-08 06:16:43 +0000

    Changed loops and logs to reflect rectangle attributes not squares

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,12 @@ const inheritSquare = require('./5-square.js');
 class Square extends inheritSquare {
   charPrint (c) {
     if (c === undefined) {
-      for (let index = 0; index < this.size; index++) {
-        console.log('X'.repeat(this.size));
+      for (let index = 0; index < this.height; index++) {
+        console.log('X'.repeat(this.width));
       }
     } else {
-      for (let index = 0; index < this.size; index++) {
-        console.log(c.repeat(this.size));
+      for (let index = 0; index < this.height; index++) {
+        console.log(c.repeat(this.width));
       }
     }
   }
