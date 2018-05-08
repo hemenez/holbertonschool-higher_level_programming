a2c4c264d424400059f7e035b91b774a506fc005
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-05-08 19:16:34 +0000

    Fixed semistandard styling

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 let request = require('request');
-myUrl = 'http://swapi.co/api/films/' + process.argv[2];
+let myUrl = 'http://swapi.co/api/films/' + process.argv[2];
 let options = {
   url: myUrl,
   method: 'GET'
-}
-request(options, function(err, res, body) {
-  let json = JSON.parse(body);
-  let { title } = json;
-  console.log(title);
+};
+request(options, function (err, res, body) {
+  if (!err) {
+    let json = JSON.parse(body);
+    let { title } = json;
+    console.log(title);
+  }
 });
